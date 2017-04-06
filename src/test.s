77ac109005faeb7cb529d7a81a2f092162a9a3c6
revaldinho <richevans@mac.com>
2017-04-06 18:12:10 +0100

    Add multibyte directive

--- src/test.s
+++ src/test.s
@@ -1,17 +1,19 @@
-TOP   ORG     0x00
-      lda.i   0x00
-      sta     RESULT
-      not     RESULT
-      sta     RESULT
-      lda.i   0x0F0
-LOOP  add.i   0x1
-      jpz     NEXT
-      jp      LOOP
-NEXT  and.i   0x33
-      jp END
-      add.i   0x01
+TOP     ORG     0x00
+        lda.i   0x00
+        sta     RESULT
+        not     RESULT
+        sta     RESULT
+        lda.i   0x00
+LOOP    add.i   0x1
+        jpz     NEXT
+        jp      LOOP
+NEXT    and.i   0x33
+        jp END
+        add.i   0x01
 
-END   halt
+END     halt
 
-      ORG 0x200
+        ORG 0x200
 RESULT  BYTE  0x00
+        BYTE 1,2,3
+        BYTE 5,6,7,8,9,10
