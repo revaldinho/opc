31fc8314b7f6e4df058ab5dda5da3273407befb1
revaldinho <richevans@mac.com>
2017-04-07 19:59:39 +0100

    Tidy up testbench

--- src/opctb.v
+++ src/opctb.v
@@ -1,45 +1,34 @@
 `timescale 1ns / 1ns
 
 module opctb();
-
   reg [7:0] mem [ 4095:0 ];
   reg clk, reset_b;
 
   wire [11:0] addr;
-  wire [7:0] data;
-  wire ceb, rnw, oeb;
+  wire rnw ;
+  wire ceb = 1'b0;
+  wire oeb = !rnw;
+  wire [7:0]  data = ( !ceb & rnw & !oeb ) ? mem[ addr ] : 8'bz ;
 
+  // OPC CPU instantiation
   opccpu  dut0_u (.address(addr), .data(data), .rnw(rnw), .clk(clk), .reset_b(reset_b));
-
-  assign ceb = 1'b0;
-  assign oeb = !rnw;
-
-  // read operations are combinatorial
-  assign data = ( !ceb & rnw & !oeb ) ? mem[ addr ] : 8'bz ;
-
 
   initial
     begin
       $dumpvars;
-      $readmemh("test.hex", mem);
+      $readmemh("test.hex", mem); // Problems with readmemb - use readmemh for now
       clk = 0;
       reset_b = 0;
-
-
       #10005 reset_b = 1;
-
-      #50000 $finish;
+      #150000 $finish;
     end
 
+  // Simple negedge synchronous memory to avoid messing with delays initially
   always @ (negedge clk)
     if (!rnw && !ceb && oeb && reset_b)
       mem[addr] <= data;
 
-  // Setup clocks - slow clock
   always
-     begin
-        #500 clk = 0;
-        #500 clk = 1;
-     end
+    #500 clk = !clk;
 
 endmodule
