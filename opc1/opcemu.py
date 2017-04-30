407c68fd8816b5501eb922e2e06e4373815c19a0
revaldinho <richevans@mac.com>
2017-04-30 22:07:31 +0100

    Implement sub call in test and python emulator

--- opc1/opcemu.py
+++ opc1/opcemu.py
@@ -11,11 +11,11 @@ dis = dict( [ (op[k],k) for k in op])
 with open(sys.argv[1],"r") as f:
     bytemem = bytearray( [ int(x,16) for x in f.read().split() ])
 
-(pc, acc, c) = (0,0,0) # initialise machine state
+(pc, acc, c, link) = (0,0,0,0) # initialise machine state
 print ("PC   : Mem   : ACC C : Mnemonic Operand\n%s" % ('-'*40))
 while True:
     opcode = (bytemem[pc] >> 4) & 0xF
-    operand_adr = (bytemem[pc] << 8 | bytemem[pc+1]) & 0x0FFF
+    operand_adr = (bytemem[pc] << 8 | bytemem[pc+1]) & 0x07FF
     if (opcode & 0x8 == 0):
         operand_data = bytemem[operand_adr]
     else:
@@ -39,6 +39,16 @@ while True:
     elif opcode in (op["jpc"], op["jpz"], op["jp"]):
         condition = (c==1) if opcode==op["jpc"] else (acc==0) if opcode==op["jpz"] else True
         pc = operand_adr if condition else pc
+    elif opcode == op["lxa"]:
+        tmp = acc
+        acc = link
+        link = tmp & 0x07
+    elif opcode == op["rts"]:
+        pc = (link << 8) | acc
+    elif opcode == op["jsr"]:
+        link = (pc >> 8) & 0x07
+        acc = pc & 0xFF
+        pc = operand_adr
     elif opcode == op["halt"]:
         print("Stopped on halt instruction at %04x" % (pc-2) )
         break
--- opc1/test.s
+++ opc1/test.s
@@ -10,15 +10,30 @@ LOOP    add.i   0x01
         jpz     NEXT
         jp      LOOP
 NEXT    and.i   0x33
-        jp END
+        jp      END
         add.i   0x01
 
-END     halt
 
+END     jsr     SUB1
+        halt
+
+MEM1    BYTE 0x00
+MEM2    BYTE 0x00
+SUB1    sta MEM1
+        lxa
+        sta MEM2
+        lda MEM2
+        lxa
+        lda MEM1
+        rts
         ORG 0x200
 RESULT  BYTE  0x00
         BYTE 1,2,3
         BYTE 5,6,7,8,9,10
         # Next BYTE must be truncated to fit
         BYTE 555
+
+
+
+
 DATAEND
