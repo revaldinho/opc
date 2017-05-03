d6bb048daada14ed8a5b1f1b24783ec0c19fa1f6
revaldinho <richevans@mac.com>
2017-05-03 21:18:28 +0100

    bug fix emu + macro asm and fib.s

--- opc1/fib.s
+++ opc1/fib.s
@@ -1,64 +1,82 @@
+MACRO INCPTR ( _p_ , _v_ )
+        lda _p_
+        and.i 0xFF # Clear carry
+        add.i _v_
+        sta _p_
+ENDMACRO
+
+MACRO ADD16 ( _data0_, _data1_, _result_)
+        lda _data0_
+        and.i 0xff   # CLC
+        add _data1_
+        sta _result_
+        lda _data0_+1
+        add _data1_+1
+        sta _result_+1
+ENDMACRO
+
+
 RETADR: BYTE 0
         BYTE 0
-        
-DATA:   BYTE 0
-        BYTE 0
-        BYTE 0
+
+DATA:   BYTE 0, 0
+        BYTE 0, 0
+        BYTE 0, 0
 
 RSPTR:  BYTE 0
 LPCTR:  BYTE 0
 
-RSPTR:  BYTE 0
-
 # Sequence of numbers will go here ... up to 0x0FF
-RSLTS:  BYTE 0     
+RSLTS:  BYTE 0
 
         # cpu starts execution at 0x100 on reset
         ORG 0x100
-        
+
         lda.i RSLTS # initialise the results pointer
         sta RSPTR
-        
-        lda.i 0x1    # initialize the data sequence with 1,1
-        sta DATA
-        sta DATA+1
 
-        lda.i 256-10 # set up a counter to do 10 iterations
+        lda.i 0x1    # initialize the data sequence with 0x0001,0x0001 (LSByte first)
+        sta DATA
+        sta DATA+2
+        lda.i 0x0
+        sta DATA+1
+        sta DATA+3
+
+        lda.i 256-20 # set up a counter to do 10 iterations
         sta LPCTR
 
 LOOP:   jsr FIB
-        lda LPCTR
-        and.i 0xFF # Clear carry
-        add.i 0x1  
-        sta LPCTR
+
+        INCPTR(LPCTR,1)
         jpz END
         jp  LOOP
 
 END:    halt
- 
-FIB:        
+
+
+FIB:
         sta RETADR
         lxa
         sta RETADR+1
-        
-        lda DATA
-        and.i 0xFF      # CLC
-        add DATA+1
-        sta DATA+2        
+
+        ADD16( DATA, DATA+2, DATA+4)
+        lda DATA+4
         sta.p RSPTR
+        INCPTR( RSPTR,1)
+        lda DATA+5
+        sta.p RSPTR
+        INCPTR ( RSPTR, 1)
 
-        lda RSPTR
-        and.i 0xFF
-        add.i 0x1
-        sta RSPTR
-
-        lda DATA+1
-        sta DATA
         lda DATA+2
+        sta DATA
+        lda DATA+3
         sta DATA+1
+        lda DATA+4
+        sta DATA+2
+        lda DATA+5
+        sta DATA+3
 
         lda RETADR+1
         lxa
         lda RETADR
         rts
-        
--- opc1/opcasm.py
+++ opc1/opcasm.py
@@ -1,45 +1,74 @@
 # python3 opcasm.py <filename.s> [<filename.hex>]
 import sys, re
 op = { "and"  :0x00, "lda":0x01,"not"  :0x02,"add":0x03, "and.i":0x10, "lda.i":0x11, "not.i":0x12,
-       "add.i":0x13, "sec":0x15,"lda.p":0x09,"sta":0x18, "sta.p":0x08, "jpc"  :0x19, "jpz"  :0x1a, 
+       "add.i":0x13, "sec":0x15,"lda.p":0x09,"sta":0x18, "sta.p":0x08, "jpc"  :0x19, "jpz"  :0x1a,
        "jp"   :0x1b, "jsr":0x1c,"rts"  :0x1d,"lxa":0x1e, "halt" :0x1f, "BYTE":0x100 }
 
-symtab = dict();
-bytemem = bytearray(2048)
+(symtab, bytemem) = (dict(), bytearray(2048))
 line_re = re.compile( '^(\w+)?:?\s*(\w+(?:\.i|\.p)?)?\s*(.*)' )
+macstart_re = re.compile("MACRO\s*(\w*)\s*?\((.*)\)", re.IGNORECASE)
+macend_re   = re.compile("ENDMACRO.*", re.IGNORECASE)
+macinst_re  = re.compile("^(?:(\w*):?)?\s*(\w+)\s*?\((.*)\)")
+
+# Pass 0 - macro expansion
+(macro, macroname, newtext)  = ( dict(), None, [])
+for line in open(sys.argv[1], "r").readlines():
+    if macstart_re.match(line) :
+        (macroname,paramstr) = (macstart_re.match(line)).groups()
+        macro[macroname] = ([x.strip() for x in paramstr.split(",")], [])
+        newtext.append("# %s" % line)
+    elif macroname and not macend_re.match(line):
+        macro[macroname][1].append(line)
+        newtext.append("# %s" % line)
+    elif macend_re.match(line):
+        macroname = None
+        newtext.append("# %s" % line)
+    elif macinst_re.match(line):
+        (label, instname, paramstr) = (macinst_re.match(line)).groups()
+        instparams = [x.strip() for x in paramstr.split(",")]
+        labeltext = ("%s:\n" % label) if label else ""
+        if instname in macro:
+            newtext.append("%s# %s" % (labeltext,line))
+            for newline in macro[instname][1]:
+                for (s,r) in zip( macro[instname][0], instparams):
+                    if s and r:
+                        newline = newline.replace(s,r)
+                newtext.append(newline)
+            newtext.append("\n")
+    else:
+        newtext.append(line)
 
 for iteration in range (0,2):     # Two pass assembly
-    with open(sys.argv[1]) as f:
-        nextmem = 0
-        for line in f.readlines():
-            bytes = []
-            gr = line_re.match( re.sub("#.*","",line) ).groups()
-            if gr[0]:
-                exec ("%s= %d" % (gr[0],nextmem), globals(), symtab )
-            if gr[1] and gr[1] == "ORG" and gr[2]:
-                nextmem = eval(gr[2],globals(),symtab)
-            elif gr[1] and gr[1] in op:
-                bytes=[0]
-                if gr[2]:
-                    if iteration==0:
-                        bytes = [0]*len(gr[2].split(","))
-                    else:
-                        try:
-                            bytes = [eval( x ,globals(), symtab) for x in gr[2].split(",")]
-                        except (ValueError, NameError):
-                            sys.exit("Error evaluating expression %s" % gr[2] )
-                if gr[1]=="BYTE":
-                    bytes = [x & 0xFF for x in bytes]
+    nextmem = 0
+    for line in newtext:
+        bytes = []
+        gr = line_re.match( re.sub("#.*","",line) ).groups()
+        if gr[0]:
+            exec ("%s= %d" % (gr[0],nextmem), globals(), symtab )
+        if gr[1] and gr[1] == "ORG" and gr[2]:
+            nextmem = eval(gr[2],globals(),symtab)
+        elif gr[1] and gr[1] in op:
+            bytes=[0]
+            if gr[2]:
+                if iteration==0:
+                    bytes = [0]*len(gr[2].split(","))
                 else:
-                    bytes = [op[gr[1]]<<3 | (bytes[0]>>8) & 0xF, bytes[0] & 0xFF]
-            elif gr[1]:
-                sys.exit("Error: unrecognized instruction %s" % gr[1])
-            if iteration > 0 :
-                for ptr in range(0,len(bytes)):
-                    bytemem[ptr+nextmem] =  bytes[ptr]
-                print("%04x  %-20s  %s" % (nextmem,
-                    ' '.join([("%02x" % i) for i in bytes]), line.rstrip()))
-            nextmem += len(bytes)
+                    try:
+                        bytes = [eval( x ,globals(), symtab) for x in gr[2].split(",")]
+                    except (ValueError, NameError):
+                        sys.exit("Error evaluating expression %s" % gr[2] )
+            if gr[1]=="BYTE":
+                bytes = [x & 0xFF for x in bytes]
+            else:
+                bytes = [op[gr[1]]<<3 | (bytes[0]>>8) & 0xF, bytes[0] & 0xFF]
+        elif gr[1]:
+            sys.exit("Error: unrecognized instruction %s" % gr[1])
+        if iteration > 0 :
+            for ptr in range(0,len(bytes)):
+                bytemem[ptr+nextmem] =  bytes[ptr]
+            print("%04x  %-20s  %s" % (nextmem,
+                ' '.join([("%02x" % i) for i in bytes]), line.rstrip()))
+        nextmem += len(bytes)
 
 print ("\nSymbol Table:\n", symtab)
 
