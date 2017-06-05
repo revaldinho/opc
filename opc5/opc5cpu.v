2c8189f126eeaa9138c5dec03977775fee804e7d
revaldinho <richevans@mac.com>
2017-06-05 21:17:37 +0100

    opc5-xp5 config

--- opc5/opc5cpu.v
+++ opc5/opc5cpu.v
@@ -2,23 +2,25 @@ module opc5cpu( inout[15:0] data, output[15:0] address, output rnw, input clk, i
    parameter FETCH0=3'h0, FETCH1=3'h1, EA_ED=3'h2, RDMEM=3'h3, EXEC=3'h4, WRMEM=3'h5;
    parameter PRED_C=15, PRED_Z=14, PINVERT=13, FSM_MAP0=12, FSM_MAP1=11;
    parameter LD=3'b000, ADD=3'b001, AND=3'b010, OR=3'b011, XOR=3'b100, ROR=3'b101, ADC=3'b110, STO=3'b111 ;
-   reg [15:0] OR_q, IR_q, PC_q, result, result_q;
+   reg [15:0] OR_q, IR_q, PC_q, result;
    (* RAM_STYLE="DISTRIBUTED" *)
    reg [15:0] GRF_q[14:0];
    reg [15:0] GRF2_q[14:0];
    reg [2:0]  FSM_q;
-   reg        C_q, zero, carry;
+   reg        C_q, Z_q, carry;
    wire [15:0] grf_dout_p2= (IR_q[7:4]==4'hF) ? PC_q: {16{(IR_q[7:4]!=4'h0)}} & GRF2_q[IR_q[7:4]];
    wire [15:0] grf_dout= (IR_q[3:0]==4'hF) ? PC_q: (GRF_q[IR_q[3:0]] & { 16{(IR_q[3:0]!=4'h0)}});
-   wire       predicate = (IR_q[PINVERT]^((IR_q[PRED_C]|C_q)&(IR_q[PRED_Z]|zero)));      // For use once IR_q loaded (FETCH1,EA_ED)
-   wire       predicate_data = (data[PINVERT]^((data[PRED_C]|C_q)&(data[PRED_Z]|zero))); // For use before IR_q loaded (FETCH0)
+   wire       predicate = (IR_q[PINVERT]^((IR_q[PRED_C]|C_q)&(IR_q[PRED_Z]|Z_q)));      // For use once IR_q loaded (FETCH1,EA_ED)
+   wire       predicate_data = (data[PINVERT]^((data[PRED_C]|C_q)&(data[PRED_Z]|Z_q))); // For use before IR_q loaded (FETCH0)
    wire [15:0] operand = (IR_q[FSM_MAP0]==1 || IR_q[FSM_MAP1]==1) ? OR_q : grf_dout_p2; // For one word instructions operand comes from GRF2
+   wire        zero = !(|result);
+       
    assign      rnw= ! (FSM_q==WRMEM) ;
    assign      data=(FSM_q==WRMEM)?grf_dout:16'bz ;
    assign      address=( FSM_q==WRMEM || FSM_q == RDMEM)? OR_q : PC_q;
    always @( * )
      begin
-        {carry, result, zero} = { C_q, 16'bx, !(|result_q) } ;
+        {carry, result} = { C_q, 16'bx} ;
         case (IR_q[10:8])
           LD  : result=operand ;
           ADD, ADC : {carry, result}= grf_dout + operand + (!IR_q[8] & C_q); // IF ADC or ADD, IR_q[8] distinguishes between them
@@ -38,8 +40,7 @@ module opc5cpu( inout[15:0] data, output[15:0] address, output rnw, input clk, i
          EA_ED  : FSM_q <= (!predicate)? FETCH0: (IR_q[FSM_MAP1]) ? RDMEM : (IR_q[10:8]==STO) ? WRMEM : EXEC;
          RDMEM  : FSM_q <= EXEC;
          EXEC   : FSM_q <= (IR_q[3:0]==4'hF)? FETCH0: (data[FSM_MAP0]) ? FETCH1: ((data[FSM_MAP1]==1) || (data[10:8]==STO)) ? EA_ED :
-                           // Always predicated  OR carry is available in this cycle but not zero
-                           ((data[15:13]==3'b110) ||  ((data[PRED_C:PRED_Z]==2'b01) && (carry ^ data[PINVERT]))) ? EXEC : EA_ED;         
+                           ((data[PINVERT]^((data[PRED_C]|carry)&(data[PRED_Z]|zero)))) ? EXEC : EA_ED;
          default: FSM_q <= FETCH0;
        endcase
    always @(posedge clk)
@@ -60,5 +61,5 @@ module opc5cpu( inout[15:0] data, output[15:0] address, output rnw, input clk, i
      if ( FSM_q == FETCH0 )        
        IR_q <= data;
      else if ( FSM_q == EXEC)
-       { C_q, result_q, GRF_q[IR_q[3:0]], GRF2_q[IR_q[3:0]], IR_q}  <= {carry, result, result, result, data };
+       { C_q, Z_q, GRF_q[IR_q[3:0]], GRF2_q[IR_q[3:0]], IR_q}  <= {carry, zero, result, result, data };
 endmodule
