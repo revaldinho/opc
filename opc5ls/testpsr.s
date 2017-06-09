350c5e7000f34418a20e84e2875f8acae397f6c4
revaldinho <richevans@mac.com>
2017-06-09 20:56:49 +0100

    improve testpsr

--- opc5ls/testpsr.s
+++ opc5ls/testpsr.s
@@ -1,32 +1,40 @@
-MACRO   PUSH( _data_, _ptr_)
-        sto   _data_,_ptr_
-        add   _ptr_,r0,1
-ENDMACRO
+    mov r11,r0,results
 
 
-    mov r10,r0,results
-    mov r1,r0    #set zero flag
-    PUSH (r1,r10)
-    psr r2,psr   #save psr
-    PUSH (r2,r10)
 
-    mov  pc, r0, L1
-L2:
-    psr  r4,psr
-    PUSH (r4,r10)
-    mov r1, r0, 0x8000
-    add r1, r1        # set C flag
-    psr r2, psr       #save flags
-    PUSH (r2,r10)
-    psr psr, r3       # restore flags
-    psr psr, r2       # restore different flags
+    add r0, r0
+    mov r1, r0     # Z=1 C=0
     psr r3, psr
-    PUSH (r3,r10)
-    halt r0,r0,0x999
+    mov r1, r0, 1  # Z=0 C=0
+    psr r4, psr
+    mov r1, r0, 0x8000
+    add r1, r1     # Z=1 C=1
+    psr r5, psr
+    mov r1, r0, 0x8000
+    add r1, r0, 0x8001 #Z=0 C=1
+    psr r6, psr
 
-L1: psr r3,psr
-    PUSH (r3,r10)
-    mov  pc, r0, L2
+    sto r6, r11
+    sto r5, r11,1
+    sto r4, r11,2
+    sto r3, r11,3
+
+    psr psr,r0  #Clear flags
+    psr psr,r6
+    psr r7, psr
+    psr psr,r5
+    psr r8, psr
+    psr psr,r4
+    psr r9, psr
+    psr psr,r3
+    psr r10, psr
+
+    sto r7, r11,4
+    sto r8, r11,5
+    sto r9, r11,6
+    sto r0, r11,7
+
+    halt r0,r0,0x999
 
 
 results:
