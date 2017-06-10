f33181acd636597036070130cb4558b8866270c7
revaldinho <richevans@mac.com>
2017-06-10 10:56:37 +0100

    update fib

--- opc5/robfib.s
+++ opc5/robfib.s
@@ -8,7 +8,7 @@ reset:
         ld.i      r11, r0, endFibbonaci  # r11 = label constant in inner loop
         ld.i      r12, r0, j0001         # r12 = label constant in inner loop
         ld.i      r1,r0
-        ld.i      r2,r10   
+        ld.i      r2,r10
         ld.i      r4,r0,results
 j0001:
         ld.i      r3,r2
@@ -22,10 +22,8 @@ j0001:
 endFibbonaci:
         sto      r1,r4
         sto      r2,r4,1
-        nc.sto   r3,r4,2
 endFibbonaci2:
         halt    r0,r0,0x999
 
         ORG 0x100
 results:
-        
