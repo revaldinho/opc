3109333a2d48475049b5c1f1b5998ce67e0f217d
revaldinho <richevans@mac.com>
2017-06-10 08:25:30 +0100

    Tidy up robfib.s, lower speed target in UCF

--- opc5ls/robfib.s
+++ opc5ls/robfib.s
@@ -4,22 +4,22 @@
         # http://anycpu.org/forum/viewtopic.php?f=3&t=376&start=60
         #
 reset:
-        mov      r11, r0, 1             # r11 = constant 1 for quick incrementing
-        mov      r12, r0, endFibbonaci  # stash label in r12
-        mov      r13, r0, j0001         # stash label in r13
+        mov      r10, r0, 1             # r10 = constant 1 for quick incrementing
+        mov      r11, r0, 0x8000        # r11 = const used in inner loop
+        
         mov      r1,r0
-        mov      r2,r11   
+        mov      r2,r10   
         mov      r4,r0,results
 j0001:
         mov      r3,r2
         add      r3,r1
-        cmp      r3,r0,0x8000
-        c.mov    pc,r12                 # r12=endFibbonaci
+        cmp      r3,r11                 # r3 > 0x8000 ?
+        c.mov    pc,r0,endFibbonaci     
         sto      r1,r4
-        add      r4,r11
+        add      r4,r10
         mov      r1,r2
         mov      r2,r3
-        mov      pc,r13                 # r13=j0001
+        mov      pc,r0,j0001
 endFibbonaci:
         sto      r1,r4
         sto      r2,r4,1
