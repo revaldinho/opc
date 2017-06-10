2b744d138a8a1b20ef2bbb6762279b3b0430f84a
revaldinho <richevans@mac.com>
2017-06-10 19:47:14 +0100

    zero terminate strings

--- opc5ls/string.s
+++ opc5ls/string.s
@@ -12,7 +12,7 @@ p2:     mov     r10, r0, S3
 p3:     mov     r10, r0, S4
         mov     r13, r0, DONE
         mov     pc,r0,printwstr
-     
+
 DONE:
         halt r0,r0,0x999
 
@@ -28,10 +28,10 @@ printbstr:  ld      r1,r10
         z.mov   pc, r0, ret
         sto     r2,r0,0xfe09
         sto     r2, r11
-        add     r11,r0,1        
+        add     r11,r0,1
         add     r10,r0,1
         mov     pc,r0,printbstr
-ret:    
+ret:
         mov     pc,r13,0
 
 printwstr:  ld      r1,r10
@@ -43,26 +43,30 @@ printwstr:  ld      r1,r10
         add     r11,r0,1
         add     r10,r0,1
         mov     pc,r0,printwstr
-wret:    
+wret:
         mov     pc,r13,0
 
-        
+
 S1:     BSTRING "Hello - this is a byte string"
+        WORD 0
 S2:     BSTRING "...and so is this"
+        WORD 0
         # Check alignment on next few
         BSTRING "1"
-        BSTRING "12"        
+        BSTRING "12"
         BSTRING "123"
         BSTRING "1234"
 
 S3:     STRING "This is a word string"
+        WORD 0
 S4:     STRING "...and so is this"
+        WORD 0
         # Check alignment on next few
         STRING "1"
-        STRING "12"        
+        STRING "12"
         STRING "123"
         STRING "1234"
-        
 
-        
-results:        
+
+
+results:
