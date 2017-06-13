ac46910acd0c4ef16b74a43e02792979814494af
revaldinho <richevans@mac.com>
2017-06-14 00:29:06 +0100

    Remove PCI_q registers/reuse dprf

--- opc5ls/opc5lscpu.v
+++ opc5ls/opc5lscpu.v
@@ -2,7 +2,7 @@ module opc5lscpu( input[15:0] din, output[15:0] dout, output[15:0] address, outp
     parameter MOV=4'h0,AND=4'h1,OR=4'h2,XOR=4'h3,ADD=4'h4,ADC=4'h5,STO=4'h6,LD=4'h7,ROR=4'h8,NOT=4'h9,SUB=4'hA,SBC=4'hB,CMP=4'hC,CMPC=4'hD,BSWP=4'hE,PSR=4'hF,RTI=17'h100FF;
     parameter FETCH0=3'h0, FETCH1=3'h1, EA_ED=3'h2, RDMEM=3'h3, EXEC=3'h4, WRMEM=3'h5, INT=3'h6 ;
     parameter P0=15, P1=14, P2=13, IRLEN=12, IRLD=16, IRSTO=17, IRGETPSR=18, IRPUTPSR=19, IRCMP=20, INT_VECTOR=16'h0002;
-    reg [15:0] OR_q, PC_q, PCI_q, result;
+    reg [15:0] OR_q, PC_q, result;
     reg [20:0] IR_q; (* RAM_STYLE="DISTRIBUTED" *)
     reg [15:0] dprf_q[15:0];
     reg [2:0]  FSM_q, PSRI_q; // Only need to store S,C,Z because SWI will be cleared and I must have been 1 on interrupt
@@ -45,19 +45,19 @@ module opc5lscpu( input[15:0] din, output[15:0] dout, output[15:0] address, outp
         OR_q <= (FSM_q == FETCH0 || FSM_q==EXEC)? 16'b0 : (FSM_q==EA_ED) ? dprf_dout_p2 + OR_q : din;
     always @(posedge clk or negedge reset_b)
         if ( !reset_b)
-            { PC_q, PCI_q, isrv_q, PSRI_q, I_q, SWI_q, S_q, C_q, Z_q} <= 41'b0;
+            { PC_q, isrv_q, PSRI_q, I_q, SWI_q, S_q, C_q, Z_q} <= 25'b0;
         else if ( FSM_q == INT )
-            { PC_q, PCI_q, isrv_q, PSRI_q } <= { INT_VECTOR, PC_q, 1'b1, S_q, C_q, Z_q} ;
+            { PC_q, isrv_q, PSRI_q } <= { INT_VECTOR, 1'b1, S_q, C_q, Z_q} ;
         else if ( FSM_q == FETCH0 || FSM_q == FETCH1 )
             PC_q <= PC_q + 1;
         else if ( FSM_q == EXEC )
             begin
-                PC_q <= ( {isrv_q,IR_q[15:0]}==RTI)? PCI_q : (IR_q[3:0]==4'hF) ? result : ((!int_b || SWI_q) & I_q & !isrv_q )? PC_q: PC_q + 1 ; //Dont incr PC if taking interrupt
+                PC_q <= ( {isrv_q,IR_q[15:0]}==RTI)? dprf_q[IR_q[3:0]] : (IR_q[3:0]==4'hF) ? result : ((!int_b || SWI_q) & I_q & !isrv_q )? PC_q: PC_q + 1 ; //Dont incr PC if taking interrupt
                 {isrv_q, SWI_q, I_q, S_q, C_q, Z_q} <= ({isrv_q,IR_q[15:0]} ==RTI)? {3'b001,PSRI_q}: {isrv_q, swi, enable_int, sign, carry, zero};
             end
     always @ (posedge clk)
-        if ( FSM_q == EXEC )
-            dprf_q[(IR_q[IRCMP])?4'b0:IR_q[3:0]] <= result ;
+        if ( FSM_q == EXEC || FSM_q == INT)
+            dprf_q[IR_q[3:0]] <= (FSM_q==INT)? PC_q: (IR_q[IRCMP])? dprf_q[IR_q[3:0]] : result ;
     always @ (posedge clk)
         if ( FSM_q == FETCH0 || FSM_q == EXEC)
             IR_q <= { ((din[11:8]==CMP)||(din[11:8]==CMPC)), {2{(din[11:8]==PSR)}} & {(din[3:0]==4'h0),(din[7:4]==4'b0)}, (din[11:8]==STO),(din[11:8]==LD), din};
