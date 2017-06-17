da3fcd85fdc4a52ddbf606fafabd78bb939c22f7
David Banks <dave@hoglet.com>
2017-06-17 15:11:26 +0100

    opc5copro: to add external memory from 0x1000 to 0xEFFF
    Change-Id: I314b4bfb2db5b8d91dc1c631c012bf34ed522f88

--- opc5ls/opc5copro/src/opc5copro.v
+++ opc5ls/opc5copro/src/opc5copro.v
@@ -1,5 +1,5 @@
 module opc5copro (
-                  
+
    // GOP signals
    input            fastclk,
    output reg [8:2] tp,
@@ -17,10 +17,10 @@ module opc5copro (
    inout            h_irq_b,
 
    // Ram Signals
-   output           ram_cs,
-   output           ram_oe,
-   output           ram_wr,
-// inout [7:0]      ram_data, // unused, commented out to avoid warnings
+   output           ram_cs_b,
+   output           ram_oe_b,
+   output           ram_we_b,
+   inout [7:0]      ram_data,
    output [18:0]    ram_addr
 );
 
@@ -41,11 +41,21 @@ module opc5copro (
    wire [7:0]       p_data_out;
 
 // ---------------------------------------------
-// internal memory wires
+// internal memory controller wires
 // ---------------------------------------------
 
-   wire             mem_cs_b;
-   wire [15:0]      mem_data_out;
+   wire             int_cs_b;
+   wire [15:0]      int_data_out;
+
+// ---------------------------------------------
+// external memory controller wires
+// ---------------------------------------------
+
+   wire             ext_cs_b;
+   wire             ext_a0;
+   reg              ext_we_b;
+   reg [7:0]        ram_data_last;
+   reg [2:0]        count;
 
 // ---------------------------------------------
 // cpu wires
@@ -57,6 +67,8 @@ module opc5copro (
    wire [15:0]      cpu_dout;
    wire             cpu_IRQ_n;
    reg              cpu_IRQ_n_sync;
+   wire             cpu_clken;
+   wire             cpu_mreq_b;
 
 // ---------------------------------------------
 // clock generator
@@ -94,11 +106,11 @@ module opc5copro (
    ram inst_mem
      (
       .din        (cpu_dout),
-      .dout       (mem_data_out),
+      .dout       (int_data_out),
       .address    (cpu_addr[12:0]),
       .rnw        (cpu_R_W_n),
       .clk        (!clk_cpu),
-      .cs_b       (mem_cs_b)
+      .cs_b       (int_cs_b)
       );
 
    opc5lscpu inst_opc5ls
@@ -110,9 +122,9 @@ module opc5copro (
       .clk        (clk_cpu),
       .reset_b    (RSTn_sync),
       .int_b      (cpu_IRQ_n_sync),
-      .clken      (1'b1),
-      .mreq_b     (),
-      .sync       ()      
+      .clken      (cpu_clken),
+      .mreq_b     (cpu_mreq_b),
+      .sync       ()
     );
 
    tube inst_tube
@@ -136,14 +148,83 @@ module opc5copro (
     );
 
 // ---------------------------------------------
-// logic
+// address decode logic
 // ---------------------------------------------
 
-   assign p_cs_b   = !(cpu_addr[15:3] == 13'b1111111011111);
+   // Tube mapped to FEF8-FEFF
+   assign p_cs_b   = !((cpu_addr[15:3] == 13'b1111111011111) && !cpu_mreq_b);
 
-   assign mem_cs_b = !p_cs_b;
+   // Internal RAM mapped to 0000:0FFF and F000:FFFF
+   assign int_cs_b = !(((cpu_addr[15:12] == 4'h0) || (cpu_addr[15:12] == 4'hF)) && !cpu_mreq_b);
 
-   assign cpu_din  =  !p_cs_b ? p_data_out : !mem_cs_b ? mem_data_out : 16'hAAAA;
+   // External RAM mapped to 1000:EFFF
+   assign ext_cs_b = !(((cpu_addr[15:12] > 4'h0) && (cpu_addr[15:12] < 4'hF)) && !cpu_mreq_b);
+
+// ---------------------------------------------
+// external memory controller
+// ---------------------------------------------
+
+   // The CPU runs at ~50MHz (20ns)
+   //
+   // When accessing byte-wide external memory, sufficient wait states
+   // need to be added to allow two slower memory cycles to happen
+   // (low byte then high byte)
+   //
+   // R1LV0408CSA-5SC RAM timings:
+   // -- Read access time is 55ns
+   //
+   // -- Min write pulse is 40ns, write happens on rising edge
+   // -- Address setup from falling edge of write is 0ns
+   // -- Address hold from rising edge of write is 0ns
+   // -- Data setup from rising edge of write is 25ns
+   // -- Address hold from rising edge of write is 0ns
+   //
+   // To err on the safe side, we allow 4 cycles for each byte access
+   //
+   // So a complete external memory access (both bytes) takes 8 cycles
+   //
+   // Which means the memory controller must insert 7 wait stats
+
+   // Count 0..7 during external memory cycles
+   always @(posedge clk_cpu)
+     if (!RSTn_sync)
+       count <= 0;
+     else if (!ext_cs_b || count > 0)
+       count <= count + 1;
+
+   // Drop clken for 7 cycles during an external memory access
+   assign cpu_clken = !(!ext_cs_b && count < 7);
+
+   // A0 = 0 for count 0,1,2,3 (low byte) and A0 = 1 for count 4,5,6,7 (high byte)
+   assign ext_a0 = count[2];
+
+   // Generate clean write co-incident with cycles 1,2 and 5,6
+   // This gives a cycle of address/data setup and
+   // Important this is a register so it is glitch free
+   always @(posedge clk_cpu)
+      if (!cpu_R_W_n && ((count == 0 && !ext_cs_b) || (count == 1) || count == 4 || count == 5))
+         ext_we_b <= 1'b0;
+      else
+         ext_we_b <= 1'b1;
+
+   // The low byte is registered at the end of cycle 3
+   // The high byte is consumed directly from RAM at the end of cycle 7
+   always @(posedge clk_cpu)
+     if (count == 3)
+        ram_data_last <= ram_data;
+
+   // Data multiplexor
+   assign cpu_din  =  !p_cs_b ? p_data_out : !int_cs_b ? int_data_out : {ram_data, ram_data_last};
+
+// ---------------------------------------------
+// external RAM
+// ---------------------------------------------
+
+   assign ram_addr = {2'b0, cpu_addr, ext_a0};
+   assign ram_cs_b = ext_cs_b;
+   assign ram_oe_b = !cpu_R_W_n;
+   assign ram_we_b = ext_we_b;
+   assign ram_data = cpu_R_W_n ? 8'bZ : ext_a0 ? cpu_dout[15:8] : cpu_dout[7:0];
 
 // ---------------------------------------------
 // synchronise interrupts
@@ -180,10 +261,6 @@ module opc5copro (
 // unused outputs
 // ---------------------------------------------
 
-   assign ram_cs   = 1'b1;
-   assign ram_oe   = 1'b1;
-   assign ram_wr   = 1'b0;
-   assign ram_addr = 19'b0;
    assign fcs      = 1'b1;
 
 endmodule
