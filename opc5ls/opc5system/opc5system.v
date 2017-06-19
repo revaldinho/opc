6b3de56de63865cd99db05d6e0578a514a5cc1d1
David Banks <dave@hoglet.com>
2017-06-19 13:52:39 +0100

    opc5system: update cpu i/f to include int_b, clken, etc
    Change-Id: I34ef6e3b727eb343696c50d3ac43d42641c3eb87

--- opc5ls/opc5system/opc5system.v
+++ opc5ls/opc5system/opc5system.v
@@ -3,16 +3,16 @@ module opc5system ( input clk, input[7:0] sw, output[7:0] led, input rxd, output
 
    // CLKSPEED is the main clock speed
    parameter CLKSPEED = 32000000;
-   
+
    // BAUD is the desired serial baud rate
    parameter BAUD = 115200;
 
    // RAMSIZE is the size of the RAM address bus
    parameter RAMSIZE = 14;
-   
+
    // Duty Cycle controls the brightness of the seven segment display (0..15)
    parameter SEVEN_SEG_DUTY_CYCLE = 0;
-      
+
    wire [15:0] cpu_din;
    wire [15:0] cpu_dout;
    wire [15:0] ram_dout;
@@ -23,26 +23,63 @@ module opc5system ( input clk, input[7:0] sw, output[7:0] led, input rxd, output
 
    wire        uart_cs_b = !({address[15:1],  1'b0} == 16'hfe08);
    wire         ram_cs_b = !(address[15:RAMSIZE] == 0);
-   
+
    // Synchronize reset
    always @(posedge clk)
         reset_b <= select;
 
    // The OPC5 CPU
-   opc5lscpu CPU ( cpu_din, cpu_dout, address, rnw, clk, reset_b);
+   opc5lscpu CPU
+     (
+      .din(cpu_din),
+      .clk(clk),
+      .reset_b(reset_b),
+      .int_b(1'b1),
+      .clken(1'b1),
+      .mreq_b(),
+      .sync(),
+      .dout(cpu_dout),
+      .address(address),
+      .rnw(rnw)
+      );
 
    // A block RAM - clocked off negative edge to mask output register
-   ram RAM (cpu_dout, ram_dout, address[RAMSIZE-1:0], rnw, !clk, ram_cs_b);
+   ram RAM
+     (
+      .din(cpu_dout),
+      .dout(ram_dout),
+      .address(address[RAMSIZE-1:0]),
+      .rnw(rnw),
+      .clk(!clk),
+      .cs_b(ram_cs_b)
+      );
 
    // A simple 115200 baud UART
-   uart #(CLKSPEED, BAUD) UART (cpu_dout, uart_dout, address[0], rnw, clk, reset_b, uart_cs_b, rxd, txd);
+   uart #(CLKSPEED, BAUD) UART
+     (
+      .din(cpu_dout),
+      .dout(uart_dout),
+      .a0(address[0]),
+      .rnw(rnw),
+      .clk(clk),
+      .reset_b(reset_b),
+      .cs_b(uart_cs_b),
+      .rxd(rxd),
+      .txd(txd)
+      );
 
    // Use the 4-digit hex display for the address
-   sevenseg #(SEVEN_SEG_DUTY_CYCLE) DISPLAY (sw[0] ? cpu_din : address, clk, an, seg);
+   sevenseg #(SEVEN_SEG_DUTY_CYCLE) DISPLAY
+     (
+      .value(sw[0] ? cpu_din : address),
+      .clk(clk),
+      .an(an),
+      .seg(seg)
+      );
 
    // Data Multiplexor
    assign cpu_din = ram_cs_b ? (uart_cs_b ? 16'hffff : uart_dout) : ram_dout;
-   
+
    // LEDs could be used for debugging in the future
    assign led = sw;
 
