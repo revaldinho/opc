56e01412845ae7bd517b6aa8c133976ace1e1c7f
revaldinho <richevans@mac.com>
2017-06-19 22:05:55 +0100

    code tidy up

--- opc5ls/opc5lscpu.v
+++ opc5ls/opc5lscpu.v
@@ -5,13 +5,12 @@ module opc5lscpu( input[15:0] din, input clk, input reset_b, input int_b, input 
     reg [15:0] OR_q, PC_q, PCI_q, result;
     reg [21:0] IR_q; (* RAM_STYLE="DISTRIBUTED" *)
     reg [15:0] dprf_q[15:0];
-    reg [2:0]  FSM_q; 
+    reg [2:0]  FSM_q;
     reg [3:0]  swiid,PSRI_q;
     reg [7:0]  PSR_q ;
     reg        zero,carry,sign,enable_int,reset_s0_b,reset_s1_b;
-    wire predicate 	      = IR_q[P2] ^ (IR_q[P1]?(IR_q[P0]?PSR_q[S]:PSR_q[Z]):(IR_q[P0]?PSR_q[C]:1));
-    wire predicate_din 	      = din[P2] ^ (din[P1]?(din[P0]?PSR_q[S]:PSR_q[Z]):(din[P0]?PSR_q[C]:1));
-
+    wire predicate 	            = IR_q[P2] ^ (IR_q[P1]?(IR_q[P0]?PSR_q[S]:PSR_q[Z]):(IR_q[P0]?PSR_q[C]:1));
+    wire predicate_din 	        = din[P2] ^ (din[P1]?(din[P0]?PSR_q[S]:PSR_q[Z]):(din[P0]?PSR_q[C]:1));
     wire [15:0] dprf_dout_p2    = (IR_q[7:4]==4'hF) ? PC_q: {16{(IR_q[7:4]!=4'h0)}} & dprf_q[IR_q[7:4]];  // Port 2 always reads source reg
     wire [15:0] dprf_dout       = (IR_q[3:0]==4'hF) ? PC_q: {16{(IR_q[3:0]!=4'h0)}} & dprf_q[IR_q[3:0]];  // Port 1 always reads dest reg
     wire [15:0] operand         = (IR_q[IRLEN]||IR_q[IRLD]) ? OR_q : dprf_dout_p2;                        // For one word instructions operand comes from dprf
@@ -30,39 +29,36 @@ module opc5lscpu( input[15:0] din, input clk, input reset_b, input int_b, input 
             {swiid,enable_int,sign,carry,zero} = (IR_q[IRPUTPSR])?operand[7:0]:(IR_q[3:0]!=4'hF)?{PSR_q[7:3],result[15],carry,!(|result)}:PSR_q;
         end // always @ ( * )
 
-    
     always @(posedge clk)
-      if (clken) begin
-          {reset_s0_b,reset_s1_b} <= {reset_b,reset_s0_b};          
-          if (!reset_s1_b)
-            {PC_q,PCI_q,PSRI_q,PSR_q,FSM_q} <= 0;
-          else begin
-            case (FSM_q)
-                FETCH0 : FSM_q <= (din[IRLEN]) ? FETCH1 : (!predicate_din) ? FETCH0 : ((din[11:8]==LD) || (din[11:8]==STO)) ? EA_ED : EXEC;
-                FETCH1 : FSM_q <= (!predicate )? FETCH0: ((IR_q[3:0]!=0) || (IR_q[IRLD]) || IR_q[IRSTO]) ? EA_ED : EXEC;
-                EA_ED  : FSM_q <= (!predicate )? FETCH0: (IR_q[IRLD]) ? RDMEM : (IR_q[IRSTO]) ? WRMEM : EXEC;
-                RDMEM  : FSM_q <= EXEC;
-                EXEC   : FSM_q <= ((!int_b & PSR_q[EI])||(IR_q[IRPUTPSR] && (|swiid)))?INT:(IR_q[3:0]==4'hF)?FETCH0: (din[IRLEN]) ? FETCH1 : // go to fetch0 if PC or PSR affected by exec
-                            ((din[11:8]==LD) || (din[11:8]==STO) ) ? EA_ED :                                       // load/store have to go via EA_ED
-                            (din[P2] ^ (din[P1] ? (din[P0] ? sign : zero): (din[P0] ? carry : 1))) ? EXEC : EA_ED; // short cut to exec on all predicates
-                            //(din[15:13]==3'b000)? EXEC : EA_ED;                                                  // or short cut on always only
-                WRMEM  : FSM_q <= (!int_b & PSR_q[EI])?INT:FETCH0;           
-                default: FSM_q <= FETCH0;
-            endcase // case (FSM_q)
-
-          OR_q <= (FSM_q == FETCH0 || FSM_q==EXEC)? 16'b0 : (FSM_q==EA_ED) ? dprf_dout_p2 + OR_q : din;
-          if ( FSM_q == INT )            
-            {PC_q,PCI_q,PSRI_q,PSR_q[EI]} <= {INT_VECTOR,PC_q,PSR_q[3:0],1'b0} ; // Always clear EI on taking interrupt
-          else if ( FSM_q == FETCH0 || FSM_q == FETCH1 )
-            PC_q <= PC_q + 1;
-          else if ( FSM_q == EXEC ) begin                    
-            PC_q <= (IR_q[IRRTI])?PCI_q:(IR_q[3:0]==4'hF)?result:((!int_b && PSR_q[EI]) || (IR_q[IRPUTPSR] && (|swiid)))?PC_q:PC_q + 1;
-            PSR_q <= (IR_q[IRRTI])?{4'b0,PSRI_q}:{swiid,enable_int,sign,carry,zero}; // Clear SWI bits on return
-            dprf_q[(IR_q[IRCMP])?4'b0:IR_q[3:0]] <= result ;
-          end
-          
-          if ( FSM_q == FETCH0 || FSM_q == EXEC)
-            IR_q <= {((din[11:8]==CMP)||(din[11:8]==CMPC)),{3{(din[11:8]==PSR)}}&{(din[3:0]==4'hF),(din[3:0]==4'h0),(din[7:4]==4'b0)},(din[11:8]==STO),(din[11:8]==LD),din};
-          end // else: !if(!reset_s1_b)
-      end
+        if (clken) begin
+            {reset_s0_b,reset_s1_b} <= {reset_b,reset_s0_b};
+            if (!reset_s1_b)
+                {PC_q,PCI_q,PSRI_q,PSR_q,FSM_q} <= 0;
+            else begin
+                case (FSM_q)
+                    FETCH0 : FSM_q <= (din[IRLEN]) ? FETCH1 : (!predicate_din) ? FETCH0 : ((din[11:8]==LD) || (din[11:8]==STO)) ? EA_ED : EXEC;
+                    FETCH1 : FSM_q <= (!predicate )? FETCH0: ((IR_q[3:0]!=0) || (IR_q[IRLD]) || IR_q[IRSTO]) ? EA_ED : EXEC;
+                    EA_ED  : FSM_q <= (!predicate )? FETCH0: (IR_q[IRLD]) ? RDMEM : (IR_q[IRSTO]) ? WRMEM : EXEC;
+                    RDMEM  : FSM_q <= EXEC;
+                    EXEC   : FSM_q <= ((!int_b & PSR_q[EI])||(IR_q[IRPUTPSR] && (|swiid)))?INT:(IR_q[3:0]==4'hF)?FETCH0: (din[IRLEN]) ? FETCH1 : // go to fetch0 if PC or PSR affected by exec
+                                ((din[11:8]==LD) || (din[11:8]==STO) ) ? EA_ED :                                       // load/store have to go via EA_ED
+                                (din[P2] ^ (din[P1] ? (din[P0] ? sign : zero): (din[P0] ? carry : 1))) ? EXEC : EA_ED; // short cut to exec on all predicates
+                                //(din[15:13]==3'b000)? EXEC : EA_ED;                                                  // or short cut on always only
+                    WRMEM  : FSM_q <= (!int_b & PSR_q[EI])?INT:FETCH0;
+                    default: FSM_q <= FETCH0;
+                endcase // case (FSM_q)
+                OR_q <= (FSM_q == FETCH0 || FSM_q==EXEC)? 16'b0 : (FSM_q==EA_ED) ? dprf_dout_p2 + OR_q : din;
+                if ( FSM_q == INT )
+                    {PC_q,PCI_q,PSRI_q,PSR_q[EI]} <= {INT_VECTOR,PC_q,PSR_q[3:0],1'b0} ; // Always clear EI on taking interrupt
+                else if ( FSM_q == FETCH0 || FSM_q == FETCH1 )
+                    PC_q <= PC_q + 1;
+                else if ( FSM_q == EXEC ) begin
+                    PC_q <= (IR_q[IRRTI])?PCI_q:(IR_q[3:0]==4'hF)?result:((!int_b && PSR_q[EI]) || (IR_q[IRPUTPSR] && (|swiid)))?PC_q:PC_q + 1;
+                    PSR_q <= (IR_q[IRRTI])?{4'b0,PSRI_q}:{swiid,enable_int,sign,carry,zero}; // Clear SWI bits on return
+                    dprf_q[(IR_q[IRCMP])?4'b0:IR_q[3:0]] <= result ;
+                end
+                if ( FSM_q == FETCH0 || FSM_q == EXEC)
+                    IR_q <= {((din[11:8]==CMP)||(din[11:8]==CMPC)),{3{(din[11:8]==PSR)}}&{(din[3:0]==4'hF),(din[3:0]==4'h0),(din[7:4]==4'b0)},(din[11:8]==STO),(din[11:8]==LD),din};
+            end // else: !if(!reset_s1_b)
+        end
 endmodule
