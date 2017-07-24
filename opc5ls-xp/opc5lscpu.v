e800f042692771401a77635b9722480db04738eb
revaldinho <richevans@mac.com>
2017-07-24 18:35:16 +0100

    Carry same perf. bug fix back to opc5ls-xp

--- opc5ls-xp/opc5lscpu.v
+++ opc5ls-xp/opc5lscpu.v
@@ -41,7 +41,7 @@ module opc5lscpu( input[15:0] din, input clk, input reset_b, input int_b, input 
                     RDMEM  : FSM_q <= EXEC;
                     EXEC   : FSM_q <= ((!int_b & PSR_q[EI])||(IR_q[IRPUTPSR] && (|swiid)))?INT:(IR_q[3:0]==4'hF)?FETCH0: (din[IRLEN]) ? FETCH1 : // go to fetch0 if PC or PSR affected by exec
                                 ((din[11:8]==LD) || (din[11:8]==STO) ) ? EA_ED :                                       // load/store have to go via EA_ED
-                                (din[P2] ^ (din[P1] ? (din[P0] ? sign : zero): (din[P0] ? carry : 1))) ? EXEC : EA_ED; // short cut to exec on all predicates
+                                (din[P2] ^ (din[P1] ? (din[P0] ? sign : zero): (din[P0] ? carry : 1))) ? EXEC : FETCH0; // short cut to exec on all predicates
                                 //(din[15:13]==3'b000)? EXEC : EA_ED;                                                  // or short cut on always only
                     WRMEM  : FSM_q <= (!int_b & PSR_q[EI])?INT:FETCH0;
                     default: FSM_q <= FETCH0;
