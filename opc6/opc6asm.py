b94f1efa08cd0d26afe3aa6d030a9cec662d8f59
revaldinho <richevans@mac.com>
2017-08-01 19:02:21 +0100

    Improved error handling

--- opc6/opc6asm.py
+++ opc6/opc6asm.py
@@ -1,7 +1,7 @@
 import sys, re
 op = "mov,and,or,xor,add,adc,sto,ld,ror,jsr,sub,sbc,inc,lsr,dec,asr,halt,bswp,putpsr,getpsr,rti,not,out,in,push,pop,cmp,cmpc".split(",")
 symtab = dict( [ ("r%d"%d,d) for d in range(0,16)] + [("pc",15), ("psr",0)])
-predicates = {"1":0x0000,"z":0x4000,"nz":0x6000,"c":0x8000,"nc":0xA000,"mi":0xC000,"pl":0xE000,"":0x0000} ##0x2000 reseved for non-predicated instructions
+pdict = {"1":0x0000,"z":0x4000,"nz":0x6000,"c":0x8000,"nc":0xA000,"mi":0xC000,"pl":0xE000,"":0x0000} ##0x2000 reseved for non-predicated instuctions
 def expand_macro(line, macro):  # recursively expand macros, passing on instances not (yet) defined
     (text,mobj)=([line],re.match("^(?P<label>\w*\:)?\s*(?P<name>\w+)\s*?\((?P<params>.*?)\)",line))
     if mobj and mobj.groupdict()["name"] in macro:
@@ -14,7 +14,7 @@ def expand_macro(line, macro):  # recursively expand macros, passing on instance
                 newline = newline.replace(s,r) if s else newline
             text.extend(expand_macro(newline, macro))
     return(text)
-(wordmem, macro, macroname, newtext,wcount) = ( [0x0000]*64*1024,dict(),None,[],0)
+(wordmem,macro,macroname,newtext,wcount,errors,warnings,reg_re)=([0x0000]*64*1024,dict(),None,[],0,[],[],re.compile("(r\d*|psr|pc)"))
 for line in open(sys.argv[1], "r").readlines():       # Pass 0 - macro expansion
     mobj =  re.match("\s*?MACRO\s*(?P<name>\w*)\s*?\((?P<params>.*)\)", line, re.IGNORECASE)
     if mobj:
@@ -31,35 +31,37 @@ for line in open(sys.argv[1], "r").readlines():       # Pass 0 - macro expansion
 for iteration in range (0,2): # Two pass assembly
     (wcount,nextmem) = (0,0)
     for line in newtext:
-        mobj = re.match('^(?:(?P<label>\w+):)?\s*((?:(?P<pred>((pl)|(mi)|(nc)|(nz)|(c)|(z)|(1)?)?)\.))?(?P<instr>\w+)?\s*(?P<operands>.*)',re.sub("#.*","",line))
-        (label, pred, instr,operands) = [ mobj.groupdict()[item] for item in ("label","pred", "instr","operands")]
+        mobj = re.match('^(?:(?P<label>\w+):)?\s*((?:(?P<pred>((pl)|(mi)|(nc)|(nz)|(c)|(z)|(1)?)?)\.))?(?P<inst>\w+)?\s*(?P<operands>.*)',re.sub("#.*","",line))
+        (label, pred, inst,operands) = [ mobj.groupdict()[item] for item in ("label","pred", "inst","operands")]
         (pred, opfields,words, memptr) = ("1" if pred==None else pred, [ x.strip() for x in operands.split(",")],[], nextmem)
-        if (label and label != "None") or (instr=="EQU"):
+        if (iteration==0 and (label and label != "None") or (inst=="EQU")):
+            errors = (errors + ["Error: Symbol %16s redefined in %s ... %s" % (label," "*14,line.strip())]) if label in symtab else errors
             exec ("%s= %s" % ((label,str(nextmem)) if label!= None else (opfields[0], opfields[1])), globals(), symtab )
-        if instr in op and iteration < 1:
-            nextmem += len(opfields)-1                  # If two operands are provide instruction will be one word
-        elif instr=="WORD" and iteration < 1:
-            nextmem += len(opfields)
-        elif instr in op or instr in ("WORD","STRING","BSTRING"):
-            if  instr=="STRING" or instr=="BSTRING":
-                (step, wordstr) =  ( 2 if instr=="BSTRING" else 1, (''.join(opfields)).strip('"')+chr(0))
-                (words) = ([(ord(wordstr[i]) | ((ord(wordstr[i+1])<<8) if instr=="BSTRING" else 0)) for  i in range(0,len(wordstr)-1,step) ])
+        if (inst=="WORD" or inst in op) and iteration < 1:
+            nextmem += len(opfields)-(inst in op)                  # If two operands are provide instuction will be one word
+        elif inst in op or inst in ("WORD","STRING","BSTRING"):
+            if  inst=="STRING" or inst=="BSTRING":
+                (step, wordstr) =  ( 2 if inst=="BSTRING" else 1, (''.join(opfields)).strip('"')+chr(0))
+                (words) = ([(ord(wordstr[i]) | ((ord(wordstr[i+1])<<8) if inst=="BSTRING" else 0)) for  i in range(0,len(wordstr)-1,step) ])
             else:
+                if ((len(opfields)==2 and not reg_re.match(opfields[1])) and inst not in ("inc","dec","WORD")):
+                    warnings.append("Warning: suspected register field missing in %s... %s" % (" "*14,line.strip()))
                 try:
                     exec("PC=%d+%d" % (nextmem,len(opfields)-1), globals(), symtab) # calculate PC as it will be in EXEC state
                     words = [eval( f,globals(), symtab) & 0xFFFF for f in opfields ];
                 except (ValueError, NameError, TypeError,SyntaxError):
-                    sys.exit("Error illegal register name or expression, or undefined symbol in: %s" % line )
-                if instr in op:
+                    (words,errors)=([0]*3,errors+["Error: illegal or undefined register name or expression in ... %s" % line.strip() ])
+                if inst in op:
                     (dst,src,val) = (words+[0])[:3]
-                    words = [((len(words)==3)<<12)|(predicates[pred] if ((op.index(instr)&0x10)==0) else 0x2000)|((op.index(instr)&0x0F)<<8)|(src<<4)|dst,val][:len(words)-(len(words)==2)]
+                    words = [((len(words)==3)<<12)|(pdict[pred] if ((op.index(inst)&0x10)==0) else 0x2000)|((op.index(inst)&0x0F)<<8)|(src<<4)|dst,val][:len(words)-(len(words)==2)]
             (wordmem[nextmem:nextmem+len(words)], nextmem,wcount )  = (words, nextmem+len(words),wcount+len(words))
-        elif instr == "ORG":
+        elif inst == "ORG":
             nextmem = eval(operands,globals(),symtab)
-        elif instr and (instr != "EQU")  :
-            sys.exit("Error: unrecognized instruction %s" % instr)
+        elif inst and (inst != "EQU")  :
+            sys.exit("Error: unrecognized instuction %s" % inst)
         if iteration > 0 :
             print("%04x  %-20s  %s"%(memptr,' '.join([("%04x" % i) for i in words]),line.rstrip()))
-print ("\nAssembled %d words of code.\n\nSymbol Table:\n\n%s\n" % (wcount, '\n'.join(["%-32s 0x%04X (%06d)" % (k,v,v) for k,v in sorted(symtab.items()) if not re.match("r\d*|pc|psr",k)])))
-with open(sys.argv[2],"w" ) as f:
+print ("\nAssembled %d words of code with %d error%s and %d warning%s." % (wcount,len(errors),'' if len(errors)==1 else 's',len(warnings),'' if len(warnings)==1 else 's'))
+print ("\nSymbol Table:\n\n%s\n\n%s\n%s" % ('\n'.join(["%-32s 0x%04X (%06d)" % (k,v,v) for k,v in sorted(symtab.items()) if not re.match("r\d*|pc|psr",k)]),'\n'.join(errors), '\n'.join(warnings)))
+with open("/dev/null" if len(errors)>0 else sys.argv[2],"w" ) as f:   ## write to hex file only if no errors else send result to null file
     f.write( '\n'.join([''.join("%04x " % d for d in wordmem[j:j+24]) for j in [i for i in range(0,len(wordmem),24)]]))
