3325002dd7fa1d506bf5df3a408dc56628d845d5
revaldinho <richevans@mac.com>
2017-08-05 23:21:04 +0100

    Add BYTE directive to assembler

--- opc6/opc6asm.py
+++ opc6/opc6asm.py
@@ -19,7 +19,7 @@ for line in open(sys.argv[1], "r").readlines():       # Pass 0 - macro expansion
     mobj =  re.match("\s*?MACRO\s*(?P<name>\w*)\s*?\((?P<params>.*)\)", line, re.IGNORECASE)
     if mobj:
         (macroname,macro[macroname])=(mobj.groupdict()["name"],([x.strip() for x in (mobj.groupdict()["params"]).split(",")],[]))
-    elif re.match("\s*?ENDMACRO.*", line, re.IGNORECASE):        
+    elif re.match("\s*?ENDMACRO.*", line, re.IGNORECASE):
         (macroname, line) = (None, '# ' + line)
     elif macroname:
         macro[macroname][1].append(line)
@@ -33,24 +33,25 @@ for iteration in range (0,2): # Two pass assembly
         if (iteration==0 and (label and label != "None") or (inst=="EQU")):
             errors = (errors + ["Error: Symbol %16s redefined in %s ... %s" % (label," "*14,line.strip())]) if label in symtab else errors
             exec ("%s= %s" % ((label,str(nextmem)) if label!= None else (opfields[0], opfields[1])), globals(), symtab )
-        if (inst=="WORD" or inst in op) and iteration < 1:
-            nextmem += len(opfields)-(inst in op)                  # If two operands are provide instuction will be one word
-        elif inst in op or inst in ("WORD","STRING","BSTRING"):
+        if (inst in("WORD","BYTE") or inst in op) and iteration < 1:
+            nextmem += (len(opfields) if inst=="WORD" else ((len(opfields)+1)//2) if inst=="BYTE" else len(opfields)-1) # If two operands are provide instuction will be one word
+        elif inst in op or inst in ("BYTE","WORD","STRING","BSTRING"):
             if  inst=="STRING" or inst=="BSTRING":
                 (step, wordstr) =  ( 2 if inst=="BSTRING" else 1, (''.join(opfields)).strip('"')+chr(0))
                 (words) = ([(ord(wordstr[i]) | ((ord(wordstr[i+1])<<8) if inst=="BSTRING" else 0)) for  i in range(0,len(wordstr)-1,step) ])
             else:
-                if ((len(opfields)==2 and not reg_re.match(opfields[1])) and inst not in ("inc","dec","WORD")):
+                if ((len(opfields)==2 and not reg_re.match(opfields[1])) and inst not in ("inc","dec","WORD","BYTE")):
                     warnings.append("Warning: suspected register field missing in %s... %s" % (" "*14,line.strip()))
                 try:
                     exec("PC=%d+%d" % (nextmem,len(opfields)-1), globals(), symtab) # calculate PC as it will be in EXEC state
-                    words = [eval( f,globals(), symtab) for f in opfields ];
+                    words = [eval( f,globals(), symtab) for f in opfields ] + ([0] if inst=="BYTE" else []) # pad out BYTE lines wih a single zero
+                    words = ([(words[i+1]&0xFF)<<8|(words[i]&0xFF) for i in range(0,len(words)-1,2)]) if inst=="BYTE" else words # pack bytes 2 to a word
                 except (ValueError, NameError, TypeError,SyntaxError):
                     (words,errors)=([0]*3,errors+["Error: illegal or undefined register name or expression in ... %s" % line.strip() ])
                 if inst in op:
                     (dst,src,val,abs_src) = (words+[0])[:3] + [words[1] if words[1]>0 else -words[1]]
                     errors=(errors+["Error: short constant out of range in%s... %s"%(' '*22, line.strip())]) if (inst in('inc','dec') and (abs_src>0xF)) else errors
-                    (inst,src) = ( 'dec',(~src +1)&0xF) if inst=='inc' and (src&0x8000) else (inst,src) #spot increment with negative immediate and swap to dec                    
+                    (inst,src) = ( 'dec',(~src +1)&0xF) if inst=='inc' and (src&0x8000) else (inst,src) #spot increment with negative immediate and swap to dec
                     words=[((len(words)==3)<<12)|(pdict[pred] if ((op.index(inst)&0x10)==0) else 0x2000)|((op.index(inst)&0x0F)<<8)|(src<<4)|dst,val&0xFFFF][:len(words)-(len(words)==2)]
             (wordmem[nextmem:nextmem+len(words)], nextmem,wcount )  = (words, nextmem+len(words),wcount+len(words))
         elif inst == "ORG":
