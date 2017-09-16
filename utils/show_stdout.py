a2a6641380bc2dd912014afc8fd75eb1331e1cac
revaldinho <richevans@mac.com>
2017-09-16 11:53:19 +0100

    show_stdout.py can take piped stdin

--- utils/show_stdout.py
+++ utils/show_stdout.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python3
+#
+# cat <trace> | show_stdout.py
+# OR
+# show_stdout.py  <filename.trace>
+#
 import re
 import sys
 
 stdout_re = re.compile("\s*?OUT:.*?Data : 0x.*? \(\s*?(\d*)\).*?")
 
-with open(sys.argv[1], "r") as f:
-    lines = f.readlines()
-    for l in lines:
+if len(sys.argv) > 1:
+    with open(sys.argv[1], "r") as f:
+        lines = f.readlines()
+        for l in lines:
+            mobj = stdout_re.match(l)
+            if mobj:
+                ch = chr( int(mobj.group(1)) )
+                print(ch, end="")
+else:
+    for l in sys.stdin:
         mobj = stdout_re.match(l)
         if mobj:
             ch = chr( int(mobj.group(1)) )
-            print(ch, end="")
-    
+            print(ch, end="")            
 
+print()
