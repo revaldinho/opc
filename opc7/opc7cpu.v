f384509a4e97ea70b11abb16954d9eca62d7bcd1
revaldinho <richevans@mac.com>
2017-10-31 22:44:47 +0000

    Reinstate pred check in FET/EXEC - cycle savings too large to lose

--- opc7/opc7cpu.v
+++ opc7/opc7cpu.v
@@ -1,8 +1,7 @@
 module opc7cpu(input[31:0] din,input clk,input reset_b,input[1:0] int_b,input clken,output vpa,output vda,output vio,output[31:0] dout,output[19:0] address,output rnw);    
   parameter MOV=5'h0,MOVT=5'h1,XOR=5'h2,AND=5'h3,OR=5'h4,NOT=5'h5,CMP=5'h6,SUB=5'h7,ADD=5'h8,BROT=5'h9,ROR=5'hA,LSR=5'hB,JSR=5'hC,ASR=5'hD,ROL=5'hE;
   parameter HLT=5'h10,RTI=5'h11,PPSR=5'h12,GPSR=5'h13,OUT=5'h18,IN=5'h19,STO=5'h1A,LD=5'h1B,LJSR=5'h1C,LMOV=5'h1D,LSTO=5'h1E,LLD=5'h1F;
-  parameter FET=3'h0,EAD=3'h1,RDM=3'h2,EXEC=3'h3,WRM=3'h4,INT=3'h5;
-  parameter EI=3,S=2,C=1,Z=0,P0=31,P1=30,P2=29,INT_VECTOR0=20'h2,INT_VECTOR1=20'h4;
+  parameter FET=3'h0,EAD=3'h1,RDM=3'h2,EXEC=3'h3,WRM=3'h4,INT=3'h5, P0=31,P1=30,P2=29,EI=3,S=2,C=1,Z=0,INT_VECTOR0=20'h2,INT_VECTOR1=20'h4;
   reg [19:0]  PC_q,PCI_q;(* RAM_STYLE="DISTRIBUTED" *)
   reg [31:0]  RF_q[14:0], RF_pipe_q, OR_q,result;
   reg [7:0]   PSR_q;
@@ -10,7 +9,8 @@ module opc7cpu(input[31:0] din,input clk,input reset_b,input[1:0] int_b,input cl
   reg [3:0]   swiid,PSRI_q,dst_q,src_q;  
   reg [2:0]   FSM_q, PRED_q;
   reg         zero,carry,sign,enable_int,reset_s0_b,reset_s1_b,subnotadd_q;
-  wire        pred          = (PRED_q[0] ^ (PRED_q[1]?(PRED_q[2]?PSR_q[S]:PSR_q[Z]):(PRED_q[2]?PSR_q[C]:1)));// New data,old flags (in fetch)  
+  wire        pred_din      = (din[P2] ^ (din[P1]?(din[P0]?PSR_q[S]:PSR_q[Z]):(din[P0]?PSR_q[C]:1))); // New data,old flags (in fetch0)
+  wire        pred_d        = (din[P2] ^ (din[P1] ? (din[P0] ? sign : zero): (din[P0] ? carry : 1))); // New data,new flags (in exec/fetch)
   wire [31:0] RF_sout       =  {32{(|src_q)&&IR_q[4:2]!=3'b111}} & ((src_q==4'hF)? {12'b0,PC_q} : RF_q[src_q]);
   assign {rnw,dout,address} = {!(FSM_q==WRM), RF_pipe_q,(FSM_q==WRM||FSM_q==RDM)? OR_q[19:0] : PC_q};
   assign {vpa,vda,vio}      = {(FSM_q==FET||FSM_q==EXEC),({2{FSM_q==RDM||FSM_q==WRM}}&{!(IR_q==IN||IR_q==OUT),IR_q==IN||IR_q==OUT})};    
@@ -29,15 +29,16 @@ module opc7cpu(input[31:0] din,input clk,input reset_b,input[1:0] int_b,input cl
   end // always @ ( * )    
   always @(posedge clk)
     if (clken) begin
-      RF_pipe_q <= (dst_q==4'hF)? {12'b0,PC_q} : RF_q[dst_q] & {32{(|dst_q)}};     
-      {reset_s0_b,reset_s1_b} <= {reset_b,reset_s0_b};
+      RF_pipe_q <= (dst_q==4'hF)? {12'b0,PC_q} : RF_q[dst_q] & {32{(|dst_q)}};
+      OR_q  <= (FSM_q==RDM)?din: (FSM_q==EAD) ? (RF_sout+OR_q) ^ {32{IR_q==SUB||IR_q==CMP}}:{(din[28:26]==3'h7)?{12'b0,din[19:16]}:{16{din[15]}}, din[15:0]};
+      {reset_s0_b,reset_s1_b, subnotadd_q} <= {reset_b,reset_s0_b, IR_q!=ADD};
       if (!reset_s1_b)
         {PC_q,PCI_q,PSRI_q,PSR_q,FSM_q} <= 0;
       else begin
         case (FSM_q)
-          FET    : FSM_q <= EAD ; 
-          EAD    : FSM_q <= (!pred)?FET:(IR_q==LD||IR_q==LLD||IR_q==IN) ? RDM : (IR_q==STO||IR_q==LSTO||IR_q==OUT) ? WRM : EXEC;     
-          EXEC   : FSM_q <= ((!(&int_b) & PSR_q[EI])||(IR_q==PPSR&&(|swiid)))?INT:(dst_q==4'hF||IR_q==JSR||IR_q==LJSR)?FET:EAD;                              
+          FET    : FSM_q <= (!pred_din) ? FET : EAD ; 
+          EAD    : FSM_q <= IR_q==LD||IR_q==LLD||IR_q==IN) ? RDM : (IR_q==STO||IR_q==LSTO||IR_q==OUT) ? WRM : EXEC;     
+          EXEC   : FSM_q <= ((!(&int_b) & PSR_q[EI])||(IR_q==PPSR&&(|swiid)))?INT:(dst_q==4'hF||(!pred_d)||IR_q==JSR||IR_q==LJSR)?FET:EAD;                              
           WRM    : FSM_q <= (!(&int_b) & PSR_q[EI])?INT:FET;                  
           default: FSM_q <= (FSM_q==RDM)? EXEC : FET;  
         endcase            
@@ -45,15 +46,13 @@ module opc7cpu(input[31:0] din,input clk,input reset_b,input[1:0] int_b,input cl
           {PRED_q,IR_q, dst_q, src_q} <= din[31:16] ;
         else if (FSM_q==EAD & IR_q==CMP )
           dst_q <= 4'b0; // Zero dest address after reading it in EAD for CMP operations                 
-        OR_q  <= (FSM_q==RDM)?din: (FSM_q==EAD) ? (RF_sout+OR_q) ^ {32{IR_q==SUB||IR_q==CMP}}:{(din[28:26]==3'h7)?{12'b0,din[19:16]}:{16{din[15]}}, din[15:0]};
-        subnotadd_q <= (IR_q!=ADD);        
         if ( FSM_q == INT )
           {PC_q,PCI_q,PSRI_q,PSR_q[EI]} <= {(!int_b[1])?INT_VECTOR1:INT_VECTOR0,PC_q,PSR_q[3:0],1'b0}; 
         else if (FSM_q==FET) 
           PC_q  <= PC_q + 1;
         else if ( FSM_q == EXEC) begin
           PC_q <= (IR_q==RTI)?PCI_q: (dst_q==4'hF) ? result[19:0] : (IR_q==JSR||IR_q==LJSR)? OR_q[19:0]:((!(&int_b)&&PSR_q[EI])||(IR_q==PPSR&&(|swiid)))?PC_q:PC_q + 1;
-          PSR_q <= (IR_q==RTI)?{4'b0,PSRI_q}:{swiid,enable_int,sign,carry,zero}; // Clear SWI bits on return
+          PSR_q <= (IR_q==RTI)?{4'b0,PSRI_q}:{swiid,enable_int,sign,carry,zero}; 
           RF_q[dst_q] <= result;
         end
       end 
