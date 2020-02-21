b107945d269f91208ded9696c96089d882785783
revaldinho <richevans@mac.com>
2020-02-21 21:56:11 +0000

    Commit OPC7 version with synchronous RAM interface for FPGA implementation (with code update for overflow flag + removal of a false RAM-RAM path) + new TB. Passes all existing tests, but code needs a bit of a crunch to get closer to OPC goals

--- opc7/opc7tb.v
+++ opc7/opc7tb.v
@@ -1,4 +1,3 @@
-
 `timescale 1ns / 1ns
 `define HALT 5'b1_0000
 `define EXEC 3'b011
@@ -15,7 +14,7 @@ module opc7tb(  ) ;
    wire          mreq_b = !(vda||vpa);
    integer       seed = 10;
    // OPC CPU instantiation
-   opc7cpu  dut0_u (.address(addr), .din(data0), .dout(data1), .rnw(rnw), .clk(clk), .reset_b(reset_b), .int_b({1'b1, interrupt_b}), .clken(clken), .vpa(vpa), .vda(vda), .vio(vio));
+   opc7cpu  dut0_u (.addr(addr), .din(data0), .dout(data1), .rnw(rnw), .clk(clk), .reset_b(reset_b), .int_b({1'b1, interrupt_b}), .clken(clken), .vpa(vpa), .vda(vda), .vio(vio));
    initial begin
 
 `ifdef _dumpvcd
@@ -32,7 +31,7 @@ module opc7tb(  ) ;
      $finish;
    end
 
-  always @ (negedge clk) begin
+  always @ (posedge clk) begin
     if (!rnw && !ceb && oeb && reset_b)
       if ( !mreq_b) begin
         mem[addr&20'hFFFFF] <= data1;
