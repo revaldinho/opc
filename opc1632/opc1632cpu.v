0f92e61c6b31fdd63f3319905692b638eb2c1d8e
revaldinho <richevans@mac.com>
2024-06-05 20:42:13 +0100

    First checkin

---
+++ opc1632/opc1632cpu.v
@@ -0,0 +1,168 @@
+module opc1632cpu(input[15:0] din,input clk,input rst_b,input[1:0] int_b,input clken,output vpa,output vda,output output[15:0] dout,output[31:0] address,output rnw);
+
+  // Non-Predicated Instructions
+  PARAMETER AND  =6'h00;
+  PARAMETER OR   =6'h01;
+  PARAMETER XOR  =6'h02;
+  PARAMETER ADD  =6'h03;
+  PARAMETER ADC  =6'h04;
+  PARAMETER SUB  =6'h05;
+  PARAMETER SBC  =6'h06;
+  PARAMETER CMP  =6'h07;
+  PARAMETER CMPC =6'h08;
+  PARAMETER ROR  =6'h09;
+  PARAMETER LSR  =6'h0A;
+  PARAMETER ASR  =6'h0B;
+  PARAMETER SXT  =6'h0C;
+  PARAMETER BROR =6'h0D;
+  PARAMETER HROR =6'h0E;
+  PARAMETER NOT  =6'h0F;
+  // NON-PREDICATED INSTRUCTIONS
+  PARAMETER HLT  =6'h10;
+  PARAMETER PPSR =6'h11;
+  PARAMETER GPSR =6'h12;
+  PARAMETER RTI  =6'h13;
+  // LSU instructions
+  PARAMETER LDW  =6'h14;
+  PARAMETER STW  =6'h15;
+  PARAMETER LDH  =6'h16;
+  PARAMETER STH  =6'h17;
+  // PREDICATED INSTRUCTIONS (LISTED WITH COND=0/LSBs=0)
+  PARAMETER JSR  =6'h20;
+  PARAMETER MOV  =6'h28;
+  PARAMETER INC  =6'h30;
+  PARAMETER INC2 =6'h38;
+  // FSM States
+  PARAMETER FET0 =5'h00;
+  PARAMETER FET1 =5'h01;
+  PARAMETER FET2 =5'h02;
+  PARAMETER EAD  =5'h03;
+  PARAMETER RD0  =5'h04;
+  PARAMETER RD1  =5'h05;
+  PARAMETER EXEC =5'h06;
+  PARAMETER WR0  =5'h07;
+  PARAMETER WR1  =5'h08;
+  PARAMETER INT  =5'h09;
+  PARAMETER WRH  =5'h10;
+  PARAMETER RDH  =5'h11;
+  // Flags
+  PARAMETER     EI =3;
+  PARAMETER     S  =2;
+  PARAMETER     C  =1;
+  PARAMETER     Z  =0;
+  // Predicate bits are 3 LSBs of the opcode field
+  PARAMETER     P0  =10;
+  PARAMETER     P1  = 9;
+  PARAMETER     P2  = 8;
+  // Misc
+  PARAMETER     INT_VECTOR0=32'h0002;
+  PARAMETER     INT_VECTOR1=32'h0004;
+  // Macros
+`define IMM6_b5 14
+`define IMM6_b4 11
+`define IS_COND_d  (din[13]  ==1'b1)
+  reg [31:0]    OR_d,OR_q,PC_d,PC_q,PCI_d,PCI_q,result,RF1_d,RF1_q;
+  (* RAM_STYLE="DISTRIBUTED" *)
+  reg [31:0]    RF_q[15:0];
+  reg [7:0]     PSR_d,PSR_q;
+  reg [5:0]     op_d,op_q;
+  reg [4:0]     FSM_q, FSM_d;
+  reg [3:0]     PSRI_d,PSRI_q,rdst_d,rdst_q,rsrc_d,rsrc_q;
+  reg           carry,rst0_qb,rst1_qb,pred_d,pred_q;
+
+  assign rnw   = !(FSM_d==WRH||FSM_d==WR0||FSM_d==WR1);
+  assign dout  = RF1_q ;
+  assign address = (FSM_d==WRH||FSM_d==RDH)? OR_q: (FSM_d==WR0||FSM_d==RD0)? OR_q & 32'hFFFFFFFE: (FSM_d==WR1||FSM_d==RD1)? OR_q | 32'h00000001: PC_q;
+  assign vpa   = (FSM_d==FET0)||(FSM_d==FET1)||(FSM_d==FET2);
+  assign vda   = (FSM_d==RDH)||(FSM_d==RD0)||(FSM_d==RD1)||(FSM_d==WRH)||(FSM_d==WR0)||(FSM_d==WR1);
+
+  always @(*) begin
+    // defaults
+    {OR_d,PC_d,RF1_d,len_d,op_d,pred_d,rdst_d,rsrc_d,PCI_d,PSRI_d,PSR_d, carry}={OR_q,PC_q,RF1_q,len_q,op_q,pred_q,rdst_q,rsrc_q,PCI_q,PSRI_q,PSR_q, PSR_q[C]};
+
+    case (op_q)
+      AND,OR     : result = (op_q==AND)?(RF1_q & OR_q):(RF1_q | OR_q);
+      ADD,ADC,INC   :{carry,result} = RF1_q + OR_q + ((op_q==ADC) & PSR_q[C]);
+      SUB,SBC,CMP,CMPC:{carry,result} = RF1_q + (OR_q ^ 32'hFFFF) + (op_q==SBC||op_q==CMPC)?PSR_q[C]:1);
+      XOR,GPSR    : result = (op_q==GPSR)?{24'b0,PSR_q}: RF1_q ^ OR_q;
+      NOT,SEXT    : result = (op_q==NOT) ? ~OR_q : {16{OR_q[15]}},OR_q[15:0];
+      BROR,HROR    : result = (op_q==BROR)? {OR_q[7:0], OR_q[31:8]}: {OR_q[15:0],OR_q[31:16]};
+      ROR,ASR,LSR   :{result,carry} = {(op_q==ROR)?PSR_q[C]:(op_q==ASR)?OR_q[31]:1'b0,OR_q};
+      default     : result = OR_q;
+      //LD,MOV,STO,JSR and everything else
+    endcase // case (op_q)
+
+    case (FSM_q)
+      FET0 : begin
+        FSM_d = (din[15]) ? FET1 : EAD;
+        PC_d  = PC_q+1 ;
+        OR_d  = (din[13:12]==INC[5:4])?{{26{din[`IMM6_b5]}},din[`IMM6_b4],din[7:4]}:32'b0;
+        len_d = din[15:14];
+        op_d  = (`IS_COND_d)? ((din[13:12]==INC[5:4])?INC : {din[13:11],3'b0}) : din[13:8];
+        // Re-map INC2->INC, Zero LSBs opcodes with cond. or imm bits
+        pred_d = (! `IS_COND_d)||(din[P2] ^ (din[P1]?(din[P0]?PSR_q[S]:PSR_q[Z]):(din[P0]?PSR_q[C]:1)));
+        // New data,old flags (in fetch0), always TRUE if non-predicated instr
+        rsrc_d = (din[13:12]==INC[5:4])? din[3:0] : din[7:4] ;
+        // INC implies Rd as source and dest
+        rdst_d = din[3:0];
+      end
+      FET1 : begin
+        FSM_d = (len_q[0]) ? FET2 : (!pred_q)? FET0 : EAD;
+        PC_d = PC_q+1;
+        OR_d = {{16{din[15]}}, din};
+      end
+      FET2  : begin
+        FSM_d = (!pred_q)? FET0 : EAD;
+        PC_d = PC_q+1;
+        OR_d = {OR_q[15:0], din};
+      end
+      EAD  : begin
+        FSM_d = (!pred_q)? ((op_q==LDH)?RDH: ((op_q==LDW)?RD0: ((op_q==STH)?WRH: ((op_q==STW)?WR0: EXEC)))): FET0;
+        OR_d = ((rsrc_q==4'hF)?PC_q: {32{(rsrc_q!=4'h0)}} & RF_q[rsrc_q]) + OR_q;
+        // Port 2 always reads source reg
+        RF1_d = ((rdst_q==4'hF)?PC_q: {32{(rdst_q!=4'h0)}} & RF_q[rdst_q]);
+        // Port 1 always reads dest reg
+      end
+      EXEC  : begin
+        PC_d = (op_q==RTI)?PCI_q: ((rdst_q==4'hF)||(op_q==JSR)) ? result: PC_q;
+        FSM_d = ((!(&int_b) & PSR_q[EI])||((op_q==PPSR) && (|swiid)))?INT: FET0;
+        PSR_d = (op_q==RTI)?{4'b0,PSRI_q}:(op_q==PPSR)?OP_q[7:0]: (rdst_q!=4'hF)? {PSR_q[7:3],result[31],carry,!(|result)}: PSR_q;
+        // Clear SWI bits on return
+      end
+      WR0  : FSM_d = WR1;
+      WR1  : FSM_d = (!(&int_b) & PSR_q[EI])?INT:FET0;
+      WRH  : FSM_d = (!(&int_b) & PSR_q[EI])?INT:FET0;
+      RD0  : begin
+        FSM_d = RD1;
+        OR_d = {{16{din[15]}}, din};
+      end
+      RD1  : begin
+        FSM_d = EXEC;
+        OR_d = {OR_q[15:0], din};
+      end
+      RDH  : begin
+        FSM_d = EXEC;
+        OR_d = {{16{din[15]}}, din};
+      end
+      INT  : begin
+        FSM_d   = FET0;
+        PC_d   = (!int_b[1])?INT_VECTOR1:INT_VECTOR0;
+        PCI_d   = PC_q;
+        PSRI_d  = PSR_q[3:0];
+        PSR_d[EI] = 1'b0;
+        // Always clear EI on taking interrupt
+      end
+      default: FSM_d = FET0;
+    endcase
+  end
+
+  always @(posedge clk)
+    if (clken) begin
+      if (!rst1_qb) {PC_q,PSR_q,FSM_q} <= {`32b0,`8b0,FET0};
+      else begin
+        {rst0_qb,rst1_qb, FSM_q, PC_q,PCI_q,PSRI_q,PSR_q} <= {rst_b,rst0_qb, FSM_d, PC_d,PCI_d,PSRI_d,PSR_d};
+        {OR_q,PC_q,RF1_q,len_q,op_q,pred_q,rdst_q,rsrc_q}={OR_d,PC_d,RF1_d,len_d,op_d,pred_d,rdst_d,rsrc_d};
+        if ( (FSM_q==EXEC) && (!((op_q==CMP)||(op_q==CMPC))) RF_q[rdst_q]<=(op_q==JSR)? PC_q : result;
+      end
+    end
+endmodule
